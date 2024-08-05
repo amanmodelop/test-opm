1119f1880981aede16c207b2168ab18c80a9c1e7
amanmodelop <156818279+amanmodelop@users.noreply.github.com>
2024-08-05 15:19:17 -0400

    Update test_tag.py

--- test_tag.py
+++ test_tag.py
@@ -32,19 +32,13 @@ def metrics(df:pd.DataFrame):
     yield opm   
 
 def main():
-
-	raw_json = Path('example_job.json').read_text()
-	init_param = {'rawJson': raw_json}
-
-	init(init_param)
-	df = pd.read_csv('german_credit_data.csv')
-	# data = '''
-	#  	{"id":993,"duration_months":36,"credit_amount":3959,"installment_rate":4,"present_residence_since":3,"age_years":30,"number_existing_credits":1,"checking_status":"A11","credit_history":"A32","purpose":"A42","savings_account":"A61","present_employment_since":"A71","debtors_guarantors":"A101","property":"A122","installment_plans":"A143","housing":"A152","job":"A174","number_people_liable":1,"telephone":"A192","foreign_worker":"A201","gender":"male","label_value":0,"score":1}
-	#  '''
-	# data_dict = json.loads(data)
-	# df = pd.DataFrame.from_dict([data_dict])
-	print(json.dumps(next(metrics(df)), indent=2))
+    raw_json=Path('example_job.json').read_text()
+    init_param={'rawJson':raw_json}
+    init(init_param)
+    data = {"data1":993,"data2":36,"data3":3959,"label_value":0,"score":1}
+    df = pd.DataFrame.from_dict([data])
+    print(json.dumps(next(metrics(df)), indent=2))
 
 
 if __name__ == '__main__':
-	main()
+	main()   
